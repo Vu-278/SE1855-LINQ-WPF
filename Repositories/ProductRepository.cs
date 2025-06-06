ab0a23945125c441169ca6781884186d05807057
Hoàng Vũ <vunlhse181695@fpt.edu.vn>
2025-06-06 09:38:49 +0700

    Initial commit

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using BusinessObjects;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ProductDAO dao = new ProductDAO();
+
+        public void DeleteProduct(Product p) => dao.DeleteProduct(p);
+        public Product GetProductById(int id) => dao.GetProductById(id);
+        public List<Product> GetProducts() => dao.GetProducts();
+        public void SaveProduct(Product p) => dao.SaveProduct(p);
+        public void UpdateProduct(Product p) => dao.UpdateProduct(p);
+
+    }
+}
